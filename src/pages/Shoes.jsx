5d33835b865284791261d013b3b6717ba2596e1e
Sumeet-source <dhakads458669@gmail.com>
2026-08-05 21:43:53 +0530

    Fixed unicode escape corruption in Shoes.jsx

--- src/pages/Shoes.jsx
+++ src/pages/Shoes.jsx
@@ -1,4 +1,4 @@
-\import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import axiosClient from '../api/axiosClient';
 import FilterDrawer from '../components/FilterDrawer';
