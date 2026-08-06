4d6cbfc433dd7a9da8ad480fc144426da9cd280c
Sumeet-source <dhakads458669@gmail.com>
2026-08-07 03:59:48 +0530

    Added flex-1 layout fix to eliminate white space permanently

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
 import { WishlistProvider } from './context/WishlistContext';
 import { ToastProvider } from './context/ToastContext';
 
@@ -22,7 +23,6 @@ import Dashboard from './pages/Dashboard';
 import Admin from './pages/Admin';
 import Wishlist from './pages/Wishlist';
 import Search from './pages/Search';
-import Checkout from './pages/Checkout';
 import ForgotPassword from './pages/ForgotPassword';
 import ResetPassword from './pages/ResetPassword';
 
@@ -30,39 +30,43 @@ function App() {
   return (
     <BrowserRouter>
       <AuthProvider>
-        <WishlistProvider>
-          <ToastProvider>
-            
-            <Navbar />
-            
-            <div className="min-h-screen flex flex-col">
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/men" element={<Men />} />
-                <Route path="/women" element={<Women />} />
-                <Route path="/shoes" element={<Shoes />} />
-                <Route path="/outlet" element={<Outlet />} />
-                <Route path="/new-arrivals" element={<NewArrivals />} />
-                <Route path="/product/:id" element={<ProductDetails />} />
-                <Route path="/cart" element={<Cart />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/admin" element={<Admin />} />
-                <Route path="/wishlist" element={<Wishlist />} />
-                <Route path="/search" element={<Search />} />
-                
-                {/* FIXED: Route */}
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/forgot-password" element={<ForgotPassword />} />
-                <Route path="/reset-password/:token" element={<ResetPassword />} />
-              </Routes>
+        <CartProvider>
+          <WishlistProvider>
+            <ToastProvider>
               
-              <Footer />
-            </div>
+              {/* 🟢 Full page flex column layout */}
+              <div className="min-h-screen flex flex-col">
+                
+                <Navbar />
+                
+                {/* 🟢 flex-1 added here: Ye saari extra space lega aur footer ko neeche push karega */}
+                <div className="flex-1">
+                  <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/men" element={<Men />} />
+                    <Route path="/women" element={<Women />} />
+                    <Route path="/shoes" element={<Shoes />} />
+                    <Route path="/outlet" element={<Outlet />} />
+                    <Route path="/new-arrivals" element={<NewArrivals />} />
+                    <Route path="/product/:id" element={<ProductDetails />} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/admin" element={<Admin />} />
+                    <Route path="/wishlist" element={<Wishlist />} />
+                    <Route path="/search" element={<Search />} />
+                    <Route path="/forgot-password" element={<ForgotPassword />} />
+                    <Route path="/reset-password/:token" element={<ResetPassword />} />
+                  </Routes>
+                </div>
 
-          </ToastProvider>
-        </WishlistProvider>
+                <Footer />
+              </div>
+
+            </ToastProvider>
+          </WishlistProvider>
+        </CartProvider>
       </AuthProvider>
     </BrowserRouter>
   );
