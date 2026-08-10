481b86095fcb8f6ca3d64cb72ceb3d4226006c41
Sumeet-source <dhakads458669@gmail.com>
2026-08-10 21:46:18 +0530

    Updated routes and main entry file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './context/AuthContext';
-import { CartProvider } from './context/CartContext';
-import { WishlistProvider } from './context/WishlistContext';
-import { ToastProvider } from './context/ToastContext';
+import { Routes, Route } from 'react-router-dom';
 
 // --- COMPONENTS ---
 import Navbar from './components/Navbar';
@@ -29,41 +25,31 @@ import NewArrivalsFixed from './pages/NewArrivalsFixed';
 
 function App() {
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <CartProvider>
-          <WishlistProvider>
-            <ToastProvider>
-              <div className="min-h-screen flex flex-col">
-                <Navbar />
-                <div className="flex-1 bg-white">
-                  <Routes>
-                    <Route path="/" element={<Home />} />
-                    <Route path="/men" element={<Men />} />
-                    <Route path="/women" element={<Women />} />
-                    <Route path="/shoes" element={<Shoes />} />
-                    <Route path="/outlet" element={<Outlet />} />
-                    <Route path="/new-arrivals" element={<NewArrivalsFixed />} />
-                    <Route path="/product/:id" element={<ProductDetails />} />
-                    <Route path="/cart" element={<Cart />} />
-                    <Route path="/login" element={<Login />} />
-                    <Route path="/signup" element={<Signup />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
-                    <Route path="/admin" element={<Admin />} />
-                    <Route path="/wishlist" element={<Wishlist />} />
-                    <Route path="/search" element={<Search />} />
-                    <Route path="/forgot-password" element={<ForgotPassword />} />
-                    <Route path="/reset-password/:token" element={<ResetPassword />} />
-                    <Route path="/checkout" element={<Checkout />} />
-                  </Routes>
-                </div>
-                <Footer />
-              </div>
-            </ToastProvider>
-          </WishlistProvider>
-        </CartProvider>
-      </AuthProvider>
-    </BrowserRouter>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <div className="flex-1 bg-white">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/men" element={<Men />} />
+          <Route path="/women" element={<Women />} />
+          <Route path="/shoes" element={<Shoes />} />
+          <Route path="/outlet" element={<Outlet />} />
+          <Route path="/new-arrivals" element={<NewArrivalsFixed />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/wishlist" element={<Wishlist />} />
+          <Route path="/search" element={<Search />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/reset-password/:token" element={<ResetPassword />} />
+          <Route path="/checkout" element={<Checkout />} />
+        </Routes>
+      </div>
+      <Footer />
+    </div>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom' // 🟢 YAHAN IMPORT KAREN
 import App from './App.jsx'
 import './styles.css'
 import { CartProvider } from './context/CartContext.jsx'
@@ -8,19 +9,20 @@ import { ToastProvider } from './context/ToastContext.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import { AdminProvider } from './context/AdminContext.jsx'
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ToastProvider>
-      <AuthProvider>
-        <AdminProvider>
-          <CartProvider>
-            <WishlistProvider>
-              <App />
-            </WishlistProvider>
-          </CartProvider>
-        </AdminProvider>
-      </AuthProvider>
-    </ToastProvider>
+    <BrowserRouter> {/* 🟢 SABSE BAHAR ROUTER RAKHA HAI */}
+      <ToastProvider>
+        <AuthProvider>
+          <AdminProvider>
+            <CartProvider>
+              <WishlistProvider>
+                <App />
+              </WishlistProvider>
+            </CartProvider>
+          </AdminProvider>
+        </AuthProvider>
+      </ToastProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
