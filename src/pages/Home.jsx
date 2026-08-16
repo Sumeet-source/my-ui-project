85960d5d06ac9514c97064f0ccdfd644e29a678b
Sumeet-source <dhakads458669@gmail.com>
2026-08-16 18:32:57 +0530

    Cropped video banner from bottom using object-position and scale to hide watermark

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -71,13 +71,10 @@ export default function Home() {
       {/* --- SINGLE STATIC VIDEO BANNER --- */}
       <section className="relative w-full min-h-[60dvh] md:min-h-[85vh] max-h-[900px] overflow-hidden bg-black">
         
-        {/* 🟢 NEW OVERLAY TO HIDE EXTENSION (Made Full Cover & Highest z-index) */}
-        <div className="absolute bottom-0 right-0 z-[9999] w-full h-[200px] bg-transparent pointer-events-auto" 
-             style={{ pointerEvents: 'auto', touchAction: 'auto' }}>
-        </div>
-
+        {/* 🟢 VIDEO CROPPED FROM BOTTOM TO HIDE WATERMARK */}
         <video 
-          className="w-full h-full object-cover" 
+          className="w-full h-full object-cover object-center"
+          style={{ objectPosition: 'center top', transform: 'scale(1.02)', transformOrigin: 'top center' }}
           autoPlay 
           loop 
           muted 
