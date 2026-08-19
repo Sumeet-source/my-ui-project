052d7260dc2502c53797fb4528e0b1b4543b3a0e
Sumeet-source <dhakads458669@gmail.com>
2026-08-19 15:58:04 +0530

    navbar

--- src/components/ForgeLogo.jsx
+++ src/components/ForgeLogo.jsx
@@ -6,7 +6,7 @@ export default function ForgeLogo() {
   return (
     <Link
       to="/"
-      className="flex items-center md:ml-8 lg:ml-16 shrink-0 select-none text-black"
+       className="flex items-center md:ml-8 lg:ml-16 shrink-0 select-none text-black md:text-white"
     >
       {/* 🟢 Ab ye ScrambleLogo use karega aur forge-logo CSS apply karega */}
       <ScrambleLogo text="FORGE" className="forge-logo" />
